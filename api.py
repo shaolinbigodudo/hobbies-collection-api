3b7ab87db23aef07b114efe773bad5354a61e67b
shaolinbigodudo <brunoedit234@gmail.com>
2024-08-06 12:23:29 -0300

    aula 1

---
+++ api.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
+    {'id': 2, 'titulo': 'O Senhor Dos Aneis', 'autor': 'J. R. R. Hugozado'},
+    {'id': 3, 'titulo': 'O Prisioneiro De Azkaban', 'autor': 'Thiagozado'},
+]
+
+@app.route('/api/livros', methods= ['GET'])
+def get_livros():
+    return jsonify(livros)
+
+@app.route('/api/livros/<int:id>', methods=['GET'])
+def get_livro(id):
+    livro = next((livro for livros in livros if livro[id] == id), None)
+    return jsonify(livro) if livro  else ('', 404)
+
+
+@app.route('/api/livros', methods=['POST'])
+def add_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(novo_livro), 201
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
